8fda0c61bf62e4a3b362532628b33a799aefd113
marynmendes <marynicolesm@gmail.com>
2025-02-04 15:30:25 -0300

    Adição da biblioteca hardware/timer, definição dos pinos de cada led e criação das variáveis para situação dos led.

--- semaforo.c
+++ semaforo.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h" //biblioteca para gereciamento de temporizadores de hardware
+
+#define led_red 13 //luz vermelha
+#define led_yellow 12 //led amarelo (na placa BitDogLab será azul)
+#define led_green 11 // luz verde
+
+//variáveis para indicar situação de cada led
+bool led_red_on = false;
+bool led_yellow_on = false;
+bool led_green_on = false;
 
 
 
